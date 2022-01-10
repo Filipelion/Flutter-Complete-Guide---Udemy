c9d732e76177e50f1908eb91e1a4398650ee7d0a
filipelion <14293424+Filipelion@users.noreply.github.com>
2022-01-10 09:38:13 -0300

    chamando funções
    chamando função de botão corretamente

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(children: [
+          Text('This is my default text'),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: answerQuestion,
+          ),
+        ]),
+      ),
     );
   }
 }
